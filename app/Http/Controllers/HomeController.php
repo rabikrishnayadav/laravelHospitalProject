9888191348553fb968d4965177a9f6b8a99d189e
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-31 20:22:38 +0545

    dashboard add for user and admin

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    function redirect(){
+
+        if(Auth::id()){
+
+            if (Auth::user()->usertype == '0') {
+                return view('user.home');
+            }else{
+                return view('admin.home');
+            }
+
+        }else{
+            return redirect()->back();
+        }
+    }
+}
