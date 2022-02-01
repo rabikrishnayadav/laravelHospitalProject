fed1d859f69b0fce3fb9ce78c3d6b975668da93a
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-01 12:07:51 +0545

    add doctors in database

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Doctor;
 class AdminController extends Controller
 {
     function addview(){
 
         return view('admin.add_doctor');
+    }
+
+    function upload(Request $request){
+
+        $doctor = new Doctor;
+
+        $image = $request->doctor_img;
+
+        $filename = time().'.'.$image->getClientoriginalExtension();
+
+        $request->doctor_img->move('doctorimage',$filename);
+
+        $doctor->image = $filename;
+
+        $doctor->name = $request->name;
+        $doctor->phone = $request->phone;
+        $doctor->room = $request->room;
+        $doctor->speciality = $request->speciality;
+        $doctor->save();
+        return redirect()->back()->with('message','Doctor Added Successfully');
     }
 }
--- resources/views/admin/add_doctor.blade.php
+++ resources/views/admin/add_doctor.blade.php
@@ -3,6 +3,12 @@
 	<head>
 		<!-- plugins:css -->
 		@include('admin.css')
+		<style type="text/css">
+			label{
+				display: inline-block;
+				width: 200px;
+			}
+		</style>
 	</head>
 	<body>
 		<div class="container-scroller">
@@ -15,7 +21,46 @@
 			
 			<!-- partial -->
 			<div class="container-fluid page-body-wrapper">
-				<h1>Add Doctors</h1>
+				<div class="container pt-5" align="center">
+				@if(session()->has('message'))
+				<div class="alert alert-success"> 
+					<button type="button" class="close" data-dismiss="alert">X</button>
+					{{session()->get('message')}}
+				</div>
+				@endif
+				<form method="post" action="{{url('upload_doctor')}}" enctype="multipart/form-data">
+					@csrf
+					<div class="p-2">
+						<label>Doctor Name</label>
+						<input type="text" name="name" placeholder="write name" class="bg-dark" required>
+					</div>
+					<div class="p-2">
+						<label>Doctor Phone</label>
+						<input type="number" name="phone" placeholder="write number" class="bg-dark" required>
+					</div>
+					<div class="p-2">
+						<label>Doctor Room No</label>
+						<input type="text" name="room" placeholder="write doctor room no" class="bg-dark" required>
+					</div>
+					<div class="p-2">
+						<label>Speciality</label>
+						<select name="speciality" class="bg-dark" required>
+							<option value="">--Select Speciality--</option>
+							<option value="skin">Skin</option>
+							<option value="heart">Heart</option>
+							<option value="eye">Eye</option>
+							<option value="nose">Nose</option>
+						</select>
+					</div>
+					<div class="p-2">
+						<label>Doctor Image</label>
+						<input type="file" name="doctor_img" class="bg-dark" required>
+					</div>
+					<div class="p-2">
+						<input type="submit" name="submit" value="Submit" class="btn btn-success">
+					</div>
+				</form>
+				</div>
 			</div>
 		</div>
 		<!-- plugins:js -->
